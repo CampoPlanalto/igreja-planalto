3f6b04ab1f14045866c08b32d0d41f33a89c3f6a
Igreja Campo do Planalto <contato@campodoplanalto.org>
2026-07-23 20:53:52 -0300

    fix: import Button from @/components/ui/Button instead of ./Button

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
-import { Button } from './Button';
+import { Button } from '@/components/ui/Button';
 import {
     LayoutDashboard,
     Users,
